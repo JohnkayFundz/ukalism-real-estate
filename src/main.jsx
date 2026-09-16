0b488e75e00d69eb2901d3847b9fcf040df638a2
John Kalumba <179124012+JohnkayFundz@users.noreply.github.com>
2026-09-16 03:15:03 +0100

    Load premium front-end refinement styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './premium.css'
 import AdminPage from './admin/AdminPage.jsx'
 
 const isAdminRoute = window.location.pathname === '/admin' ||
